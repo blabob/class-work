d08028e62343fb6975aadd1ce7d5d2a1ab405cb9
Bob <blabob@uw.edu>
2018-02-05 15:16:38 -0800

    Add class files

---
+++ WriteFile.py
@@ -0,0 +1,8 @@
+fout = open('data\output.txt', 'w')
+print(fout)
+
+line1 = "This here's the wattle,\n"
+fout.write(line1)
+line2 = 'the emblem of our land.\n'
+fout.write(line2)
+fout.close()
---
+++ open2.py
@@ -0,0 +1,4 @@
+fhand = open('data\mbox-short.txt')
+inp = fhand.read()
+print(len(inp))
+print(inp[:25])
